2d6ded22303083a1c79105a9661868e9f0c801bd
BeatDiaz <beatdiazmusic@gmail.com>
2025-05-27 16:17:53 +0300

    Factory enemy

---
+++ FactoryEnemy.cs
@@ -0,0 +1,14 @@
+﻿using Commandos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos.Models
+{
+    public class FactoryEnemy
+    {
+        public static List<Enemy> enemies = new List<Enemy> { };           
+    }
+}
