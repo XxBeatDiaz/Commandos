ebb12af01118e4ce805d52b6b0790d0de3b8fdf0
BeatDiaz <beatdiazmusic@gmail.com>
2025-05-27 17:20:24 +0300

    stone wepon

--- Stone.cs
+++ Stone.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Commandos
+namespace Commandos.Models
 {
-    class Stone
+    public class Stone
     {
+        string Name;
+        double Weight;
+        string Color;
+        bool Status;
+        int Hits;
+        int RemainingHits;
+
+        public Stone(string name, double weight, string color, bool status = true, int hits = 5)
+        {
+            Name = name;
+            Weight = weight;
+            Color = color;
+            Status = status;
+            Hits = hits;
+            RemainingHits = 0;
+        }
     }
 }
