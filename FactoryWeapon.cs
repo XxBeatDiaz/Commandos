3990b4824eb488c3b0bc03d8022b4985c5eb7055
BeatDiaz <beatdiazmusic@gmail.com>
2025-05-27 16:20:48 +0300

    Factory weapons

---
+++ FactoryWeapon.cs
@@ -0,0 +1,14 @@
+﻿using Commandos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos.Models
+{
+    public class FactoryWeapon
+    {
+        public static List<Weapon> weapons = new List<Weapon> { };
+    }
+}
